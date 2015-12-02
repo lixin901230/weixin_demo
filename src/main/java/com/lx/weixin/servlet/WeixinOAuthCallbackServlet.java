28aa39bcee71cc0b1a86d04381daba3255f58fab
lixin901230 <412506897@qq.com>
2015-12-03 01:32:27 +0800

    微信 第三方网页授权 demo 微信关注时间消息推送

---
+++ src/main/java/com/lx/weixin/servlet/WeixinOAuthCallbackServlet.java
@@ -0,0 +1,36 @@
+package com.lx.weixin.servlet;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 微信第三方网页授权回调
+ * 授权回调地址：https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect
+ * 
+ * 网页授权流程详见微信文档：http://mp.weixin.qq.com/wiki/9/01f711493b5a02f24b04365ac5d8fd95.html
+ * 用户管理——》网页授权获取用户基本信息
+ */
+public class WeixinOAuthCallbackServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public static void main(String[] args) throws Exception {
+		String url = URLEncoder.encode("https://www.lixinsj.com.cn/weixin/wxOAuthCallback.do", "UTF-8");
+		System.out.println(url);
+	}
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost");
+		
+	}
+	
+}
