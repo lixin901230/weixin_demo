57b42f8278b428374f432fc5091c10f18e4fb5a2
lixin901230 <412506897@qq.com>
2015-12-03 23:24:49 +0800

    微信demo数据库

---
+++ src/main/webapp/db/weixin_demo.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Community v11.11 (64 bit)
+MySQL - 5.1.73-community : Database - weixin_demo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`weixin_demo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `weixin_demo`;
+
+/*Table structure for table `user_info` */
+
+DROP TABLE IF EXISTS `user_info`;
+
+CREATE TABLE `user_info` (
+  `id` varchar(50) NOT NULL COMMENT '主键',
+  `userName` varchar(50) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(50) DEFAULT NULL COMMENT '密码',
+  `trueName` varchar(50) DEFAULT NULL COMMENT '真实姓名',
+  `unionId` varchar(50) DEFAULT NULL COMMENT '微信用户唯一标示',
+  `nickName` varchar(50) DEFAULT NULL COMMENT '昵称',
+  `sex` varchar(10) DEFAULT NULL COMMENT '性别',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(50) DEFAULT NULL COMMENT '手机号',
+  `validFlag` varchar(4) DEFAULT NULL COMMENT '有效标记，0：无效；1：有效；2：挂起',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+
+/*Data for the table `user_info` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
